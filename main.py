09c741b4a59b1da16607636ca89cd6c996b3a7fe
mohammedAbubaker <m.abubaker1@hotmail.com>
2024-04-30 12:31:21 +0100

    added stuff

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from tkinter import *
+import tkinter as tk
+import tkinter as tk					 
+from tkinter import ttk 
 
-root = Tk()
 
-a = Label(root, text="Hello world")
+root = tk.Tk() 
+root.title("Music Player") 
+tabControl = ttk.Notebook(root) 
 
-a.pack()
+tab1 = ttk.Frame(tabControl) 
+tab2 = ttk.Frame(tabControl)
+tab3 = ttk.Frame(tabControl) 
 
-root.mainloop()
+tabControl.add(tab1, text ='Artists') 
+tabControl.add(tab2, text ='Albums') 
+tabControl.add(tab3, text ='Songs') 
+
+tabControl.pack(expand = 1, fill ="both") 
+
+ttk.Label(tab1, 
+		text = '''Welcome to
+		GeeksForGeeks''').grid(column = 0, 
+							row = 0, 
+							padx = 30, 
+							pady = 30) 
+ttk.Label(tab2, 
+		text = '''Lets dive into the\ 
+		world of computers''').grid(column = 0, 
+									row = 0, 
+									padx = 30, 
+									pady = 30) 
+
+root.mainloop() 
 
